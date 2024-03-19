f94b72c6001eddee974adcf929f551c66ba1df30
santino-rosso <sl.rosso@alumno.um.edu.ar>
2024-03-19 19:28:34 -0300

    Primer item terminado

---
+++ Practico-01/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Ingrediente ingrediente1 = new Ingrediente("Harina", 500);
+        Ingrediente ingrediente2 = new Ingrediente("Azúcar", 250);
+        Ingrediente ingrediente3 = new Ingrediente("Huevos", 6);
+
+        System.out.println("Ingredientes:");
+        System.out.println(ingrediente1);
+        System.out.println(ingrediente2);
+        System.out.println(ingrediente3);
+    }
+}
